9df187dab66d5577e8824a55147b7c302380bbfe
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2026-04-15 12:20:13 -0300

    feat: adding auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,41 @@
-const login = (req, res) => {
-  // Lógica de autenticación aquí
-  res.send("Login Route");
-}
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const { UnauthenticatedError } = require("../errors");
 
-const register = (req, res) => {
-  // Lógica de registro aquí
-  res.send("Register Route");
-};  
+const login = async (req, res) => {
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new BadRequestError("Invalid Credentials");
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createToken();
+
+  res.status(StatusCodes.OK).json({ name: user.name, token });
+
+};
+
+const register = async (req, res) => {
+  console.log(req.body);
+  const user = await User.create(req.body);
+  console.log(`Aqui ${user}`);
+  const token = user.createToken();
+  res.status(StatusCodes.OK).json({ name: user.name, token });
+};
 
 module.exports = {
   login,
-  register
-}
+  register,
+};
--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 require("express-async-errors");
+require("dotenv").config();
 const express = require("express");
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
 const notFound = require("./middlewares/notFound");
-const errorHandler = require("./middlewares/errorHandler");
+const errorHandler = require("./middlewares/errorHandler")
+const connectDB = require("./db/connect");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+start();
