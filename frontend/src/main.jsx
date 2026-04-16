3e8c632b92a8631e3faa6a99deb9a410655045f2
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2026-04-15 21:48:21 -0300

    feat: frontend working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,5 +3,5 @@ import router from './router'
 import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
+  <RouterProvider router={router}></RouterProvider>
 )
